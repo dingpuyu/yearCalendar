04fff8d417b87bc45b465bcfef8e11f7c4d1ffc4
dingpuyu <771005227@qq.com>
2015-12-01 15:13:32 +0800

    Initial Commit

---
+++ 年历/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  年历
+//
+//  Created by xiaotei's on 15/12/1.
+//  Copyright © 2015年 xiaotei's. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 年历/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  年历
+//
+//  Created by xiaotei's on 15/12/1.
+//  Copyright © 2015年 xiaotei's. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
